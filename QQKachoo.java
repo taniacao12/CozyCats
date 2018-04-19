39e7c74db935ba99f257c6709d6a1299b585f955
taniacao12 <33487802+taniacao12@users.noreply.github.com>
2018-04-19 09:00:01 -0400

    Update QQKachoo.java

--- QQKachoo.java
+++ QQKachoo.java
@@ -1,157 +1,119 @@
-// Team CozyCats -- Tania Cao, Puneet Johal, Frank Chen
-// APCS2 pd2
-// L02 -- All Hands on Deque!
-// 2018-04-18
-
-public class QQKachoo<T> implements Deque<T>
-{
+public class QQKachoo<T> implements Deque<T> {
 	// Instance Variables
 	private DLLNode<D> _front, _end;
 	
 	// Default Constructor
-	public QQKachoo()
-	{
+	public QQKachoo() {
 		_front = null;
 		_end = null;
 	}
 	
 	// Check If Empty
-	public boolean isEmpty()
-	{
-		if( _front == null )
-		{
+	public boolean isEmpty() {
+		if (_front == null)
 			return true;
-		}
 		return false;
 	}
 	
 	// Add Front
-	public void addFirst( T x )
-	{
-		if( _front == null )
-		{
-			DLLNode<T> node = new DLLNode( x, null );
+	public void addFirst (T x) {
+		if (_front == null) {
+			DLLNode<T> node = new DLLNode(x, null);
 			_front = node;
 			_end = node;
 		}
-		else
-		{
-			DLLNode<T> node = new DLLNode( x, _front );
-			_front.setPrev( node );
+		else {
+			DLLNode<T> node = new DLLNode(x, _front);
+			_front.setPrev(node);
 			_front = node;
 		}
 	}
 	
 	// Add End
-	public void addLast( T x )
-	{
-		if( _front == null )
-		{
-			DLLNode<T> node = new DLLNode( x, null );
+	public void addLast (T x) {
+		if (_front == null) {
+			DLLNode<T> node = new DLLNode(x, null);
 			_front = node;
 			_end = node;
 		}
-		else
-		{
-			DLLNode<T> node = new DLLNode( x, null );
-			_end.setNext( node );
-			node.setPrev( _end );
+		else {
+			DLLNode<T> node = new DLLNode(x, null);
+			_end.setNext(node);
+			node.setPrev(_end);
 			_end = node;
 		}
 	}
 	
 	// 
-	public void offerFirst( T x )
-	{
+	public void offerFirst (T x) {
 		
 	}
 
 	// 
-    public void offerLast( T x )
-	{
+	public void offerLast (T x) {
 		
 	}
 	
 	// Remove Front
-    public T removeFirst()
-	{
-		if( isEmpty() )
-		{
+	public T removeFirst() {
+		if (isEmpty())
 			return null;
-		}
-		T temp = ( T )peekFirst();
+		T temp = (T) peekFirst();
 		_front = _front.getNext();
-		_front.setPrev( null );
+		_front.setPrev(null);
 		return temp;
 	}
 
 	// Remove Last
-    public T removeLast()
-	{
-		if( isEmpty() )
-		{
+	public T removeLast() {
+		if (isEmpty())
 			return null;
-		}
-		T temp = ( T )peekLast();
-		if( _front.equals( _end ) )
-		{
+		T temp =  (T) peekLast();
+		if (_front.equals(_end))
 			_front = _end = null;
-		}
-		else
-		{
+		else {
 			_end = _end.getPrev();
-			_end.setNext( new DLLNode( null, null ) );
+			_end.setNext(new DLLNode(null, null));
 		}
 		return temp;
 	}
 
 	// 
-    public T pollFirst()
-	{
+	public T pollFirst() {
 		
 	}
 	
 	// 
-    public T pollLast()
-	{
+	public T pollLast() {
 		
 	}
 
 	// 
-    public T getFirst()
-	{
+	public T getFirst() {
 		
 	}
 
 	// 
-    public T getLast()
-	{
+	public T getLast() {
 		
 	}
 
 	// Look At First
-    public T peekFirst()
-	{
-		if( !isEmpty() )
-		{
-			return ( T )_front.toString();
-		}
+	public T peekFirst() {
+		if (!isEmpty())
+			return (T) _front.toString();
 		return null;
 	}
 
 	// Look At Last
-    public T peekLast()
-	{
-		if( !isEmpty() )
-		{
-			return ( T )_end.toString();
-		}
+	public T peekLast() {
+		if (!isEmpty())
+			return (T) _end.toString();
 		return null;
 	}
 	
 	// Main Method
-	public static void main( String[] args )
-	{
+	public static void main (String[] args) {
 		
 	} // end main
 	
